77fd8a8c9b2247fd8f521ea373c0fd679a6c4315
Kevin James <kevin.al.james@gmail.com>
2020-06-18 10:25:46 +0300

    added welcome navigator and biolerplate code for the screens

--- app/screens/RegisterScreen/index.tsx
+++ app/screens/RegisterScreen/index.tsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View, StatusBar } from 'react-native'
-import KelvinLogoSvg from '../../assets/svg/Logo'
+import { View, StatusBar, Text } from 'react-native'
 
 import * as theme from '../../theme'
 
 const VIEW_STYLE = { flex: 1, justifyContent: 'center', alignItems: 'center' }
 const COLORED_VIEW_STYLE = { ...VIEW_STYLE, backgroundColor: '#009245' }
 
-/**
- * TODO: shift this to using `react-native-splash-screen` instead
- */
-export default function SplashScreen() {
+export default function RegisterScreen() {
   return (
     <SafeAreaView style={COLORED_VIEW_STYLE}>
       <StatusBar barStyle='default' backgroundColor={theme.color.app.statusBar} />
       <View style={VIEW_STYLE}>
-        {/* Main App Logo */}
-        <KelvinLogoSvg />
+        <Text>This is the Registration Screen</Text>
       </View>
     </SafeAreaView>
   )
