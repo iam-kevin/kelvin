a8b159d69fed256e559acbeca572615af6379def
Kevin James <kevin.al.james@gmail.com>
2020-07-02 00:03:09 +0300

    change from root store to mobx-state-tree format

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,161 +1,112 @@
 import { computed, action, observable, IObservableArray } from "mobx"
 import { GiftedChat, IMessage } from "react-native-gifted-chat"
+import { types, onSnapshot } from "mobx-state-tree"
+import { normalizeMessage } from "../../context/chat-context"
 
-import io from 'socket.io-client'
-import { fromString } from 'uuidv4'
-import { act } from "react-test-renderer"
-
-export const initialMessages: Array<IMessage> = [
-  {
-    _id: 2,
-    text: 'Ungependa kujua nini?',
-    createdAt: new Date(),
-    quickReplies: {
-      type: 'radio', // or 'checkbox',
-      keepIt: true,
-      // @ts-ignore
-      position: 'right',
-      values: [
-        {
-          title: 'Hali ya hewa',
-          value: 'weather',
-        },
-        {
-          title: 'Shida ya mazao yangu',
-          value: 'plant_disease',
-        },
-        {
-          title: 'hali zipi zintakiwezesha nioteshe',
-          value: 'crop_yield',
-        },
-      ],
-    },
-    user: {
-      _id: 2,
-      name: 'Kelvin',
-    },
-  },
-  {
-    _id: 1,
-    text: 'Za kwako Kevin James. Mimi ni K.E.L.V.I.N.',
-    createdAt: new Date(),
-    user: {
-      _id: 2,
-      name: 'Kelvin',
-    },
-  },
-]
-
-function normalizeMessage(message: PMessage, sender: SenderType): IMessage {
-  const { text, createdAt } = message
-
-  return {
-    _id: sender === 'kelvin' ? this.selfId : this.userId,
-    text,
-    createdAt: createdAt === undefined ? new Date() : createdAt,
-    system: message.type === 'system'
-  } as IMessage
+export interface Reply {
+  title: string;
+  value: string;
+  messageId?: any;
+}
+export interface QuickReplies {
+  type: 'radio' | 'checkbox';
+  values: Reply[];
+  keepIt?: boolean;
 }
 
-function normalizeMessageToServer(message: IMessage): PMessage {
-  const { text, createdAt } = message
+/**
+ * Custom chat object for
+ * mobx-state-tree store
+ */
+const ChatMessage = types.custom<string, IMessage>({
+  name: "ChatMessage",
+  fromSnapshot(value: string): IMessage {
+    return JSON.parse(value) as IMessage
+  },
+  toSnapshot(value: IMessage): string {
+    return JSON.stringify(value)
+  },
+  isTargetType(value: IMessage | string) {
+    return !(typeof value === 'string')
+  },
+  getValidationMessage(value: string): string {
+    let message: IMessage
 
-  return {
-    text,
-    type: message.system === true ? 'system' : 'normal',
-    createdAt
-  } as PMessage
-}
+    try {
+      message = JSON.parse(value)
+    } catch (e) {
+      return `Unable to parse message: ${value}`
+    }
 
-export class RootStore {
-  /**
-   * TODO: make the messages get pulled up from the apps state store
-   */
-  messages: IObservableArray<IMessage> = observable(initialMessages)
-
-  selfId: string | number
-  userId: string | number
-  chatSocket: SocketIOClient.Socket
-
-  constructor() {
-    this.selfId = 1 // fromString('kelvin')
-    this.userId = 2 // fromString('user')
-
-    // connect to the server related to python
-    this.chatSocket = io.connect('http://178.62.101.62:5000')
-    this.chatSocket.on('connect', () => {
-      console.info('Connected to KELVIN')
-    })
+    if (message.text !== undefined && message.createdAt !== undefined && message.user !== undefined) return ""
+    return `Doesn't seem that the object is a IMessage\n${value}`
   }
+})
+
+/**
+ * Store
+ */
+const RootStoreModel = types.model("RootStore", {
+  messages: types.array(ChatMessage),
+}).actions(self => ({
+  /**
+   * Sending the message
+   */
+  sendMessage: (newMessage: PMessage, sender: SenderType, ...GiftedChatOptions) => {
+    const message: IMessage = normalizeMessage(newMessage, sender)
+
+    // update the message
+    self.messages.replace(GiftedChat.append(self.messages, [message], ...GiftedChatOptions))
+  },
 
   /**
-   * Sending a single message
-   * @param message message from user
-   * @param GiftedChatOptions (optional). Options specific to `GiftedChat.append` method
+   * Update
    */
-  // @action.bound
-  // private sendMessage(message: PMessage, sender: SenderType, ...GiftedChatOptions) {
-  //   // Convert message to normal message
-  //   const _msg = this.normalizeMessage(message, sender)
-
-  //   // add to message board
-  //   this.updateMessageBoard([_msg], ...GiftedChatOptions)
-  // }
-
-  // @action.bound
-  // private updateMessageBoard(newMessages: IMessage[], ...GiftedChatOptions) {
-  //   this.messages.replace(GiftedChat.append(this.messages, newMessages, ...GiftedChatOptions))
-  // }
-
-  @action
-  registerOnConnect(renderCallback: (message: IMessage) => void) {
-    const message = normalizeMessage({
-      text: 'You are connected',
-      type: 'system'
-    }, 'kelvin')
-    this.chatSocket.on('connect', function () {
-      renderCallback(message)
-
-      console.log('Connected to the host')
-    })
+  updateMessageBoard: (newMessages: IMessage[], ...GiftedChatOptions) => {
+    // update the message
+    self.messages.replace(GiftedChat.append(self.messages, newMessages, ...GiftedChatOptions))
   }
+}))
 
-  @action
-  registerOnDisconnect(renderCallback: (message: IMessage) => void) {
-    const message = normalizeMessage({
-      text: 'Disconnected from server',
-      type: 'system'
-    }, 'kelvin')
-    this.chatSocket.on('disconnect', function () {
-      renderCallback(message)
-      console.log('Disconnected from the host')
-    })
-  }
-
-  @action
-  registerOnReply(renderCallback: (message: IMessage) => void) {
-    this.chatSocket.on('reply', function (message) {
-      const normMessage: IMessage = normalizeMessage(message, 'user')
-      renderCallback(normMessage)
-    })
-  }
-
-  sendToServer(message: IMessage) {
-    const normMsg = normalizeMessageToServer(message)
-    this.chatSocket.emit('message', normMsg)
-  }
-
-  // @action.bound
-  // onSend(messages: IMessage[]) {
-  //   // Send data to server
-  //   messages.forEach(message => {
-  //     const normMessage = this.normalizeMessageToServer(message)
-  //     console.log(normMessage)
-
-  //     this.chatSocket.emit('message', normMessage)
-  //   })
-
-  //   // update the board
-  //   this.updateMessageBoard(messages)
-  // }
-}
+const rootStoreInstance = RootStoreModel.create({
+  messages: [
+    {
+      _id: 2,
+      text: 'Ungependa kujua nini?',
+      createdAt: new Date(),
+      quickReplies: {
+        type: 'radio', // or 'checkbox',
+        keepIt: true,
+        // @ts-ignore
+        position: 'right',
+        values: [
+          {
+            title: 'Hali ya hewa',
+            value: 'weather',
+          },
+          {
+            title: 'Shida ya mazao yangu',
+            value: 'plant_disease',
+          },
+          {
+            title: 'hali zipi zintakiwezesha nioteshe',
+            value: 'crop_yield',
+          },
+        ],
+      },
+      user: {
+        _id: 2,
+        name: 'Kelvin',
+      },
+    },
+    {
+      _id: 1,
+      text: 'Za kwako Kevin James. Mimi ni K.E.L.V.I.N.',
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'Kelvin',
+      },
+    },
+  ]
+})
